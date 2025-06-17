63c47e20b1941c061e78979ecf53eadea1130f6f
xiaoshuangdashu <159227073+xiaoshuangdashu@users.noreply.github.com>
2025-06-17 19:18:36 +0800

    Update TV云播.js

--- TV云播.js
+++ TV云播.js
@@ -3,6 +3,6 @@
         "线路数组": "module-tab-item tab-item\" data-dropdown-value=&&</div>",
          "线路标题": "裁缝大叔专线",
          "副标题":"裁缝在线+module-item-note&&</div>",
-      "分类url": "http://www.tianyidigital.com/vodshow/{cateId}-{area}-------{catePg}---{year}.html",
+      "分类url": "http://www.tianyidigital.com/vtype/{cateId}.html",
  "分类": "电影$1#电视剧$2#综艺$3#动漫$4"
       }
