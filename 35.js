2f41201094a4fc08b6e4b5ec8419a35e972f9e5c
xiaoshuangdashu <159227073+xiaoshuangdashu@users.noreply.github.com>
2025-06-23 18:03:49 +0800

    Update 35.js

--- 35.js
+++ 35.js
@@ -1,6 +1,6 @@
 var rule = {
 	title: '555影视[V2]', // csp_AppYsV2
-	host: 'https://iy132aiuv1qg.top',
+	host: 'https://www.guangyingmi.com',
 	homeUrl:'/api.php/app/index_video',
 	// url: '/api.php/app/video?tid=fyclass&class=&area=&lang=&year=&limit=20&pg=fypage',
 	url: '/api.php/app/video?tid=fyclassfyfilter&limit=20&pg=fypage',
