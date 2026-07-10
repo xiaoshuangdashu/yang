12c94c3e2ba5216a22b3a81955c41fa2ea47cf77
xiaoshuangdashu <159227073+xiaoshuangdashu@users.noreply.github.com>
2026-07-10 12:27:10 +0800

    Update 555影视[V2026].js

--- 555影视[V2026].js
+++ 555影视[V2026].js
@@ -1,7 +1,7 @@
 ﻿
 var rule = {
 	title: '555影视[V2]', // csp_AppYsV2
-	host: 'https://www.dmt0p7w9o3.shop/',
+	host: 'https://5k28.top/',
 	homeUrl:'/api.php/app/index_video',
 	// url: '/api.php/app/video?tid=fyclass&class=&area=&lang=&year=&limit=20&pg=fypage',
 	url: '/api.php/app/video?tid=fyclassfyfilter&limit=20&pg=fypage',
